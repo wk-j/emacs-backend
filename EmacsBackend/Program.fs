e4e30c7619990662f8bfd7a29f01d16b7f260956
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-04-16 04:42:40 +0700

    Installed Suave

--- EmacsBackend/Program.fs
+++ EmacsBackend/Program.fs
@@ -1,6 +1,18 @@
-module EmacsBackend
+module EmacsBackend.Program
+
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+
+let app =
+    choose
+      [ GET >=> choose
+          [ path "/hello" >=> OK "Hello GET"
+            path "/goodbye" >=> OK "Good bye GET"]]
 
 [<EntryPoint>]
 let main argv =
     printfn "%A" argv
+    startWebServer defaultConfig app
     0 // return an integer exit code
